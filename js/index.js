37fc3851cfecd0064c3353f8be479e7044bafe71
bbalganjjm <bbalganjjm@nate.com>
2014-12-23 15:24:46 +0900

    .

---
+++ js/index.js
@@ -0,0 +1,20 @@
+var IndexController = {
+	initWebfont : function(window) {
+		window.WebFontConfig = {
+			custom : {
+				families : [ 'Nanum Gothic' ],
+				urls : [ 'http://fonts.googleapis.com/earlyaccess/nanumgothic.css' ]
+			}
+		};
+		var wf = document.createElement('script');
+		wf.src = ('https:' == document.location.protocol ? 'https' : 'http')
+				+ '://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+		wf.type = 'text/javascript';
+		wf.async = 'true';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(wf, s);
+	},
+	init : function() {
+
+	}
+}
