22dc9ec154d6e43521e7859f518e9cd1ff4ce7fe
JakarataKim <JakarataKim@DESKTOP-2HGPIG5>
2017-03-10 17:14:06 +0900

    .

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 var IndexController = {
 	init : function(window) {
+		this.loadNanumGothicFont();
 		this.setLocale();
 		this.googleAnalytics();
 		this.loadHeader();
@@ -8,6 +9,26 @@ var IndexController = {
 		if(N.locale() === "en_US") {
 			this.notice();
 		}
+	},
+	/**
+	 * 웹폰트
+	 */
+	loadNanumGothicFont : function() {
+		WebFontConfig = {
+			custom: {
+				families: ['Nanum Gothic'],
+				urls: ['http://fonts.googleapis.com/earlyaccess/nanumgothic.css']
+			}
+		};
+		(function() {
+			var wf = document.createElement('script');
+			wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+				'://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+			wf.type = 'text/javascript';
+			wf.async = 'true';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(wf, s);
+		})();
 	},
 	/**
 	 * Google Analytics
