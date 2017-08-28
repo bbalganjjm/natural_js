b6944921d38432e0d7f2d4328b96308b558b624b
81700561 <81700561@5F012017050213X.IDBINS.CO.KR>
2017-08-28 15:31:40 +0900

    Upgraded the version(0.8.1.10) according to the function addition.

--- src/natural.architecture.js
+++ src/natural.architecture.js
@@ -1,5 +1,5 @@
 /*!
- * Natural-ARCHITECTURE v0.8.1.9
+ * Natural-ARCHITECTURE v0.8.1.10
  * bbalganjjm@gmail.com
  *
  * Copyright 2014 KIM HWANG MAN
@@ -8,7 +8,7 @@
  * Date: 2014-09-26T11:11Z
  */
 (function(window, $) {
-	N.version["Natural-ARCHITECTURE"] = "0.8.1.9";
+	N.version["Natural-ARCHITECTURE"] = "0.8.1.10";
 
 	$.fn.extend($.extend(N.prototype, {
 		ajax : function(opts) {
