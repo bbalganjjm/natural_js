c2990ea2ed2ba2660bdb19d99c984ba640ff475a
정인혜 <@DESKTOP-G286TPE>
2017-10-17 12:39:22 +0900

    N.form : fixed a bug that bind method does not bind click event to radio and checkbox when dynamic changed select options

--- src/natural.ui.js
+++ src/natural.ui.js
@@ -1,5 +1,5 @@
 /*!
- * Natural-UI v0.8.19.16
+ * Natural-UI v0.8.19.17
  * bbalganjjm@gmail.com
  *
  * Copyright 2014 KIM HWANG MAN
@@ -8,7 +8,7 @@
  * Date: 2014-09-26T11:11Z
  */
 (function(window, $) {
-	N.version["Natural-UI"] = "0.8.19.16";
+	N.version["Natural-UI"] = "0.8.19.17";
 
 	$.fn.extend($.extend(N.prototype, {
 		alert : function(msg, vars) {
