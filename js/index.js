1f7482e6a54bf52b68031668d4b5f7661e4893b2
JakarataKim <JakarataKim@DESKTOP-2HGPIG5>
2018-04-06 18:01:08 +0900

    .

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ var IndexController = {
 		if(!window.localStorage.colorTheme) {
 			window.localStorage.colorTheme = "teal";
 		}
-		//this.reloadCss();
+		this.reloadCss();
 		//this.loadWebFont();
 		this.loadHeader();
 		this.loadFooter();
@@ -38,8 +38,8 @@ var IndexController = {
 		deepOrange : ["#FBE9E7", "#FFCCBC", "#FFAB91", "#FF8A65", "#FF7043", "#FF5722", "#F4511E", "#E64A19", "#D84315", "#BF360C", "#FF9E80", "#FF6E40", "#FF3D00", "#DD2C00"],
 		brown : ["#EFEBE9", "#D7CCC8", "#BCAAA4", "#A1887F", "#8D6E63", "#795548", "#6D4C41", "#5D4037", "#4E342E", "#3E2723", "", "", "", ""],
 		grey : ["#FAFAFA", "#F5F5F5", "#EEEEEE", "#E0E0E0", "#BDBDBD", "#9E9E9E", "#757575", "#616161", "#424242", "#212121", "", "", "", ""],
-		blueGrey : ["#ECEFF1", "#CFD8DC", "#B0BEC5", "#90A4AE", "#78909C", "#607D8B", "#546E7A", "#455A64", "#37474F", "#263238", "", "", "", ""],
-		blackWhite : ["#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "", "", "", ""]
+		blueGrey : ["#ECEFF1", "#CFD8DC", "#B0BEC5", "#90A4AE", "#78909C", "#607D8B", "#546E7A", "#455A64", "#37474F", "#263238", "", "", "", ""] // ,
+		// blackWhite : ["#000000", "#000000", "#000000", "#000000", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "#FFFFFF", "", "", "", ""]
 	},
 	/**
 	 * CSS 를 다시 불러오면 filter 에서 컬러값을 치환 함.
