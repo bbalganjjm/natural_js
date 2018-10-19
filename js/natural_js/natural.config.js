527bd9e4ba6f344cd1fda7334fe0962430534246
81801210 <81801210@5F012015050216X.IDBINS.CO.KR>
2018-10-19 16:07:35 +0900

    .

--- js/natural_js/natural.config.js
+++ js/natural_js/natural.config.js
@@ -601,6 +601,15 @@
 					"confirm" : "OK",
 					"cancel" : "Cancel"
 				}
+			},
+			"onShowG" : function() {
+				N(".header nav").css("z-index", "0");
+			},
+			"onOkG" : function() {
+				N(".header nav").css("z-index", "");
+			},
+			"onCancelG" : function() {
+				N(".header nav").css("z-index", "");
 			}
 		},
 		"button" : {
@@ -608,7 +617,19 @@
 		},
 		"popup" : {
 			"draggable" : true,
-			"alwaysOnTop" : true
+			"alwaysOnTop" : true,
+			"onOpenG" : function() {
+				N(".header nav").css("z-index", "0");
+			},
+			"onCloseG" : function() {
+				N(".header nav").css("z-index", "");
+			},
+			"onOkG" : function() {
+				N(".header nav").css("z-index", "");
+			},
+			"onCancelG" : function() {
+				N(".header nav").css("z-index", "");
+			}
 		},
 		"tab" : {
 			onActive : function(tabIdx, tabEle, contentEle, tabEles, contentEles) {
