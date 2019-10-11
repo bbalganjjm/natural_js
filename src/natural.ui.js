838018c153753de2777856e5f88c8a58a95821f8
81801210 <81801210@5F012015050216X.IDBINS.CO.KR>
2019-10-11 15:36:30 +0900

    Changed the logic that is processed when the tabScroll option is enabled.

--- src/natural.ui.js
+++ src/natural.ui.js
@@ -1,5 +1,5 @@
 /*!
- * Natural-UI v0.38.217
+ * Natural-UI v0.38.218
  *
  * Released under the LGPL v2.1 license
  * Date: 2014-09-26T11:11Z
@@ -7,7 +7,7 @@
  * Copyright 2014 KIM HWANG MAN(bbalganjjm@gmail.com)
  */
 (function(window, $) {
-    N.version["Natural-UI"] = "0.38.217";
+    N.version["Natural-UI"] = "0.38.218";
 
     $.fn.extend($.extend(N.prototype, {
         alert : function(msg, vars) {
@@ -2752,7 +2752,7 @@
                         } else {
                             tabContainerEle.parent(".tab_native_scroll__").animate({ 
                                 scrollLeft: 0 
-                                }, 300, "swing");
+                            }, 300, "swing");
                         }
                     });
                     nextBtnEle = scrollBtnEles.eq(1).addClass("tab_scroll_next__").css("right", 0).bind("click" + eventNameSpace,  function(e) {
@@ -2766,7 +2766,7 @@
                         } else {
                             tabContainerEle.parent(".tab_native_scroll__").animate({ 
                                 scrollLeft: tabContainerEle.outerWidth()
-                                }, 300, "swing");
+                            }, 300, "swing");
                         }
                     });
 
@@ -2806,17 +2806,14 @@
                         
                         tabContainerEle.addClass("tab_scroll__").width(ulWidth);
                     } else {
+                        if(scrollBtnEles.length > 1) {
+                            scrollBtnEles.hide();
+                            tabContainerEle.css("margin-left", "");
+                            prevBtnEle.removeClass("disabled__");
+                        }
+                        
                         if(N.browser.scrollbarWidth() > 0) {
                             opts.context.css("overflow", "");
-                            if(scrollBtnEles.length > 1) {
-                                tabContainerEle.css("margin-left", "");
-                                prevBtnEle.removeClass("disabled__");
-                                scrollBtnEles.hide();
-                            }
-                        } else {
-                            if(scrollBtnEles.length > 1) {
-                                scrollBtnEles.hide();
-                            }
                         }
                         
                         if(tabContainerEle.parent().hasClass("tab_native_scroll__")) {
