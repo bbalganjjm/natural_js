9aafc97544fa572c79f426dc1c9e6bf1a6bcbf7a
HwangManKim <bbalganjjm@gmail.com>
2020-06-26 16:52:43 +0900

    .

--- js/common.js
+++ js/common.js
@@ -27,16 +27,16 @@
             "COMM-0010" : "No rows selected."
         }
     }
-    
+
     var CommonUtils = {
         /**
          * N.grid 나 N.list 에서 체크(check)되거나 선택(select) 된 행들을 삭제하기 위한 함수 - 삭제 전 동의 메시지 다이얼로그 표시 후 remove 함수 호출 등의 반복적인 루틴들을 한번에 처리 해 줍니다.
-         * 
+         *
          * @this : 호출한 함수 인스턴스
          * @opts.cont : N.cont object
          * @opts.inst : 행을 삭제 할 N.grid 인스턴스 명,
          * @opts.before : 선택한 행을 삭제하기 전 실행 할 함수. 핸들러 함수의 인자로 체크한 행의 index 들을 반환 합니다.
-         * @opts.after : 선택한 행을 삭제 한 후 실행 할 함수. 
+         * @opts.after : 선택한 행을 삭제 한 후 실행 할 함수.
         */
         del : function(opts) {
             var checkedIndexs = opts.cont[opts.inst].check();
@@ -46,7 +46,7 @@
             if(opts.before) {
                    opts.before.call(opts.cont, checkedIndexs);
                }
-            
+
             if(checkedIndexs.length > 0) {
                 var isAllAddedLine = true;
                 $(checkedIndexs).each(function() {
@@ -54,25 +54,25 @@
                         isAllAddedLine = false;
                     }
                 });
-                
+
                 if(isAllAddedLine) {
                     opts.cont[opts.inst].remove(checkedIndexs);
-                    
+
                     if(opts.after) {
                         opts.after.call(opts.cont);
-                    }             
+                    }
                 } else {
                     N(window).alert({
                         msg : N.message.get(APP.comm.messages, "COMM-0009"),
                         confirm : true,
                         onOk : function() {
                             opts.cont[opts.inst].remove(checkedIndexs);
-                            
+
                             if(opts.after) {
                                 opts.after.call(opts.cont);
                             }
                         }
-                    }).show(); 
+                    }).show();
                 }
             } else {
                 N(window).alert(N.message.get(APP.comm.messages, "COMM-0010")).show();
@@ -80,22 +80,22 @@
         },
         /**
          * 추가, 수정, 삭제된 데이터를 저장 하는 함수 - 저장 전 데이터 검증, 변경 된 데이터 확인, 저장 메시지 다이얼로그 표시등 데이터 저장에 대한 반복적인 루틴들을 한번에 처리 해 줍니다.
-         * 
+         *
          * @this : 호출한 함수 인스턴스
          * @opts.cont : N.cont object
          * @opts.comm : 데이터 저장을 처리하는 N.comm 이 정의 된 함수명.
          * @opts.msg : 저장 확인 메시지, undefined 이면 기본 메시지가 표시 됨.
          * @opts.changed : 변경 된 데이터 유무를 참조 할 컴포넌트 인스턴스 명.
-         * @opts.validate : 추가/수정 된 데이터의 유효성을 검증할 컴포넌트 인스턴스 명. 
-         * @opts.before : 서버에 저장 하기 전 실행 할 함수. 
-         * @opts.after : 서버에 저장 한 후 실행 할 함수. 
+         * @opts.validate : 추가/수정 된 데이터의 유효성을 검증할 컴포넌트 인스턴스 명.
+         * @opts.before : 서버에 저장 하기 전 실행 할 함수.
+         * @opts.after : 서버에 저장 한 후 실행 할 함수.
          */
         save : function(opts) {
                if(opts.changed) {
                    if(opts.cont[opts.changed].data("modified").length === 0) {
                        N.notify.add(N.message.get(APP.comm.messages, "COMM-0001"));
                     return false;
-                }                   
+                }
                }
 
                if(opts.validate) {
@@ -104,7 +104,7 @@
                         if(!opts.cont[opts.validate].validate()) {
                             return false;
                         }
-                    }                       
+                    }
                    } else {
                        if(!opts.cont[opts.validate].validate()) {
                         return false;
@@ -115,7 +115,7 @@
                if(opts.before) {
                    opts.before.call(opts.cont);
                }
-               
+
             N(window).alert({
                 msg : opts.msg ? opts.msg : N.message.get(APP.comm.messages, "COMM-0005"),
                 confirm : true,
@@ -127,75 +127,73 @@
                             msg += "<br>" + N.message.get(APP.comm.messages, "COMM-0007", [data.update]);
                             msg += "<br>" + N.message.get(APP.comm.messages, "COMM-0008", [data.delete]);
                         }
-                        
+
                         N.notify({
                             html : true
                         }).add(msg);
-                        
+
                         opts.after.call(opts.cont, data);
                     });
                 }
             }).show();
-           },
-           /**
-            * N.grid 나 N.list 의 행을 선택했을 때(onSelect 이벤트 핸들러 함수 이용) N.form 컴포넌트에 같은 데이터를 연동하기 위한 반복적인 루틴들을 한번에 처리 해 줍니다.
-            * 
-            * @this : 호출한 함수 인스턴스 - onSelect 함수의 this 이므로 N.grid나 N.list 인스턴스
-            * @opts.cont : N.cont object
-         * @opts.form : 데이터를 연동 할 N.form 인스턴스 명.
-         */
-           selectNBind : function(opts) {
-               if(opts.args === undefined) {
-                   opts.args = arguments.callee.caller.arguments; 
-               }
-               if((opts.args[2][opts.args[0]] !== undefined && opts.args[2][opts.args[0]].rowStatus !== "insert") && opts.cont[opts.form] > -1  && !opts.cont[opts.form].validate()) {
-                   return false;
-               }
+        },
+        /**
+        * N.grid 나 N.list 의 행을 선택했을 때(onSelect 이벤트 핸들러 함수 이용) N.form 컴포넌트에 같은 데이터를 연동하기 위한 반복적인 루틴들을 한번에 처리 해 줍니다.
+        *
+        * @this : 호출한 함수 인스턴스 - onSelect 함수의 this 이므로 N.grid나 N.list 인스턴스
+        * @opts.cont : N.cont object
+        * @opts.form : 데이터를 연동 할 N.form 인스턴스 명.
+        */
+        selectNBind : function(opts) {
+            if(opts.args === undefined) {
+                opts.args = arguments.callee.caller.arguments;
+            }
+            if((opts.args[2][opts.args[0]] !== undefined && opts.args[2][opts.args[0]].rowStatus !== "insert") && opts.cont[opts.form] > -1  && !opts.cont[opts.form].validate()) {
+                return false;
+            }
 
             if(this.context("> " + (this instanceof N.grid ? "tbody" : "li") + ":eq(" + opts.args[3] + ")").hasClass("row_data_changed__")) {
                 if(opts.args[0] !== opts.cont[opts.form].row()) {
                     var self = this;
                     N(window).alert({
-                          msg : N.message.get(APP.comm.messages, "COMM-0003"),
-                           confirm : true,
-                           onOk : function() {
-                               // bind data to detail form;
-                               if(opts.dataSync === false) {
-                                   opts.cont[opts.form].bind(0, [opts.args[2][opts.args[0]]]);
-                               } else {
-                                   opts.cont[opts.form].bind(opts.args[0], opts.args[2]);
-                               }
-                               opts.args[1].click();
-                           },
-                           onCancel : function() {
-                               self.select(opts.args[3]);
-                           }
-                       }).show();
+                        msg : N.message.get(APP.comm.messages, "COMM-0003"),
+                        confirm : true,
+                        onOk : function() {
+                        // bind data to detail form;
+                        if(opts.dataSync === false) {
+                            opts.cont[opts.form].bind(0, [opts.args[2][opts.args[0]]]);
+                        } else {
+                            opts.cont[opts.form].bind(opts.args[0], opts.args[2]);
+                        }
+                        opts.args[1].click();
+                        },
+                        onCancel : function() {
+                            self.select(opts.args[3]);
+                        }
+                    }).show();
                 }
-              } else {
-                  if(opts.dataSync === false) {
-                      opts.cont[opts.form].bind(0, [opts.args[2][opts.args[0]]]);
-                  } else {
-                      opts.cont[opts.form].bind(opts.args[0], opts.args[2]);
-                  }
-              }
-           },
-           /**
+            } else {
+                if(opts.dataSync === false) {
+                    opts.cont[opts.form].bind(0, [opts.args[2][opts.args[0]]]);
+                } else {
+                    opts.cont[opts.form].bind(opts.args[0], opts.args[2]);
+                }
+            }
+        },
+        /**
          * 엑셀 다운로드
-         * 
-         * 
          */
         excelDownload : function(params, url, filename, columnNames, extColumnNames) {
             if(filename === undefined) {
                 return N.error("엑셀 파일명(arguments[0]) 을 입력 하세요.");
             }
-          
+
             if(params) {
                 N.browser.cookie("n-excel-params", btoa(encodeURIComponent(JSON.stringify(params))));
             }
-            
+
             N.browser.cookie("n-excel-filename", btoa(encodeURIComponent(filename)));
-            
+
             if(columnNames instanceof N.grid) {
                 columnNamesObj = {};
                 $(columnNames.tableMap.thead).each(function() {
@@ -206,27 +204,27 @@
                         thClone.find(">*").remove();
                         var colName = thClone.text();
                         if(!N.string.isEmpty(id)) {
-                            columnNamesObj[encodeURIComponent(id)] = encodeURIComponent(thClone.text()); 
+                            columnNamesObj[encodeURIComponent(id)] = encodeURIComponent(thClone.text());
                         }
                     });
-                });  
+                });
                 columnNames = columnNamesObj;
             } else {
                 for(var k in columnNames) {
                     var v = columnNames[k];
                     delete columnNames[k];
-                    columnNames[encodeURIComponent(k)] = encodeURIComponent(v); 
+                    columnNames[encodeURIComponent(k)] = encodeURIComponent(v);
                 }
             }
-            
+
             if(extColumnNames) {
                 for(var k in extColumnNames) {
                     var v = extColumnNames[k];
                     delete extColumnNames[k];
-                    extColumnNames[encodeURIComponent(k)] = encodeURIComponent(v); 
+                    extColumnNames[encodeURIComponent(k)] = encodeURIComponent(v);
                 }
             }
-            
+
             if(columnNames !== undefined) {
                 var nExcelColumnNames = btoa(JSON.stringify($.extend(columnNames, extColumnNames)));
                 var maxCookieLength = 2048;
@@ -234,15 +232,15 @@
                     N.browser.cookie("n-excel-column-names-" + String(i), nExcelColumnNames.substring(i * maxCookieLength, (i+1) * maxCookieLength));
                 }
             }
-            
+
             CommonUtils.fileDownload(url);
         },
-           /**
-            * 엑셀 대용량 데이터 다운로드
-            * 
-            * 대용량 엑셀 데이터 조회시 Heap 메모리 Full 을 발생시키지 않고 빠르게 다운로드 함. 그러나
-            * Service 나 Controller 에서 파라미터는 조작 가능하지만 리턴 데이터는 MyBatis 에서 엑셀파일을 바로 생성하기 때문에 조작이 불가능함. 
-            */
+        /**
+         * 엑셀 대용량 데이터 다운로드
+         *
+         * 대용량 엑셀 데이터 조회시 Heap 메모리 Full 을 발생시키지 않고 빠르게 다운로드 함. 그러나
+         * Service 나 Controller 에서 파라미터는 조작 가능하지만 리턴 데이터는 MyBatis 에서 엑셀파일을 바로 생성하기 때문에 조작이 불가능함.
+         */
         excelStreaming : function(params, url, filename, columnNames, extColumnNames) {
                N.browser.cookie("n-excel-stream", btoa("true"));
                CommonUtils.excelDownload(params, url, filename, columnNames, extColumnNames);
@@ -262,9 +260,9 @@
                 var fileListStr = N.formatter.limit(N(fileList).map(function() {
                     return this[fileNameCol];
                 }).get().join(", "), [length, "..."]);
-                
+
                 fileListStr += "(" + fileList.length + ")";
-                
+
                 if(fileButton) {
                     fileButton.siblings(".fileSummaryList").remove();
                     fileButton.after('<span class="fileSummaryList">' + fileListStr + '</span>');
@@ -273,11 +271,11 @@
                 }
             } else {
                 if(fileButton) {
-                    fileButton.next(".fileSummaryList").text("(0)");                    
+                    fileButton.next(".fileSummaryList").text("(0)");
                 }
             }
-           },
-           /**
+        },
+        /**
          * fileId 로 서버에서 업로드 된 파일을 조회 후 파일 요약 목록을 만들어 준다.
          * @fileId : 파일 아이디
          * @fileNameCol : 파일명 컬럼명
@@ -290,14 +288,14 @@
             });
         }
     }
-    
+
     if(!window.APP) {
         window.APP = {};
     }
     window.APP.comm = CommonController;
     window.APP.comm.utils = CommonUtils;
     window.APP.comm.messages = CommonMessages;
-    
+
     // N.comm 에 Excel 다운로드 기능 확장
     N.comm.excelDownload = function(args) {
         if(args === undefined || args.length === 0) {
@@ -325,10 +323,10 @@
         CommonUtils.excelStreaming.call(this, this.request.options.data ? JSON.parse(this.request.options.data) : undefined, url, args[0], args[1], args[2]);
         return this;
     };
-    
+
     /**
      * N.grid 에 Excel 업로드 기능 확장
-     * 
+     *
      * header : JSON Object 키값 - 엑셀 컬럼 순서대로 JSON Object 의 키값을 정의(필수)
      * opts : 추가 옵션 오브젝트(선택)
      *  - start : 데이터로 추출 할 시작 엑셀 행 인덱스
@@ -339,7 +337,7 @@
      */
     N.grid.prototype.bindExcel = function(header, opts) {
         var self = this;
-        
+
         if(opts === undefined) {
             opts = {};
         }
@@ -349,11 +347,11 @@
         if(opts.mode === undefined) {
             opts.mode = "update";
         }
-        
+
         var fileInput = N('<input type="file" name="xlsxFile" class="excel_import__" style="display: none;" />').appendTo(self.context());
-        
+
         var rABS = !N.browser.is("ie");
-        
+
         if(opts.mode === "update" && !$.isEmptyObject(opts.pk)) {
             var gridDataStr = JSON.stringify(self.options.data.get());
             var i=0;
@@ -361,7 +359,7 @@
                 if(self.options.data.length === 0) {
                     return false;
                 }
-                
+
                 var keyObj;
                 var isDupKeys = [];
                 $(pk).each(function(i, k) {
@@ -378,20 +376,20 @@
                         N.warn("데이터에 PK 로 지정한 \"" + k + "\" 컬럼이 없습니다.");
                     }
                 });
-                
+
                 if(isDupKeys.join(",").indexOf("false") < 0) {
                     isDupKeys = undefined;
-                    return true;                        
+                    return true;
                 } else {
                     isDupKeys = undefined;
                     return false;
                 }
             };
         }
-        
+
         fileInput.on("change", function(e) {
             var files = this.files;
-            
+
             if(opts.server) {
                 var formData = new FormData();
                 formData.append("xlsxFile", files[0]);
@@ -399,7 +397,7 @@
                     formData.append("header", header);
                 }
                 formData.append("start", opts.start);
-                
+
                 opts.header = header;
                 N.comm({
                     url: "file/getDataListFromXlsx.json",
@@ -423,23 +421,23 @@
                             }
                         }
                     });
-                    isDupPk = undefined;                        
-                    
+                    isDupPk = undefined;
+
                     self.bind(data);
-                    
+
                     if(opts.after) {
                         opts.after.call(self, data);
                     }
                 });
                 return false;
             }
-            
+
             var i,f;
             for (i = 0; i != files.length; ++i) {
                 f = files[i];
                 var reader = new FileReader();
                 var name = f.name;
-                
+
                 var fixdata = function(data) {
                     var o = "", l = 0, w = 10240;
                     for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)));
@@ -449,22 +447,22 @@
 
                 reader.onload = function(e) {
                     var data = e.target.result;
-         
+
                     var workbook;
-         
+
                     if(rABS) {
                         workbook = XLSX.read(data, {type: 'binary'});
                     } else {
                         var arr = fixdata(data);
                         workbook = XLSX.read(btoa(arr), {type: 'base64'});
                     }
-         
+
                     if(workbook.SheetNames && workbook.SheetNames.length > 0) {
                         var json = XLSX.utils.sheet_to_json(workbook.Sheets[workbook.SheetNames[0]], {
                             range : opts.start,
                             header : header
                         });
-                    
+
                         $(json).each(function(i, rowData) {
                             if(opts.mode === "insert" || $.isEmptyObject(opts.pk)) {
                                 rowData.rowStatus = "insert";
@@ -476,15 +474,15 @@
                                 }
                             }
                         });
-                        
+
                         gridDataStr = undefined;
-                        
+
                         self.bind(json);
-                        
+
                         fileInput.remove();
                     };
                 };
-         
+
                 if(rABS) {
                     reader.readAsBinaryString(f);
                 } else {
@@ -492,8 +490,8 @@
                 }
             }
         });
-        
+
         fileInput.click();
     }
-    
+
 })(window, N);
