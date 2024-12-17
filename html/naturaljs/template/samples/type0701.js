d499ffff6f50e7e3bdd6a5c94f21f6959eef1ed6
Goldman Kim <bbalganjjm@gmail.com>
2024-12-17 15:21:56 +0900

    Add TypeScript integration and enhance Natural-JS typing
    Introduced TypeScript configuration with `tsconfig.json` and added type definitions for Natural-JS controllers, templates, and UI components. Removed obsolete JavaScript-based type representations and replaced inline scripts with modular TypeScript files for better manageability and maintainability.

---
+++ html/naturaljs/template/samples/type0701.js
@@ -0,0 +1,34 @@
+"use strict";
+(function () {
+    const cont = N(".type0701").cont({
+        "p.select.gender": ["gender"],
+        "p.select.eyeColor": ["eyeColor"],
+        "p.form.search": {
+            "usage": "search-box"
+        },
+        "p.tab.master": {
+            "tabScroll": true
+        },
+        "e.btnSearch.click": function (e) {
+            e.preventDefault();
+            // 활성화된 탭 페이지에서 컨트롤러 가져오기
+            const cCont = cont["p.tab.master"].cont();
+            cCont["p.form.search"].bind(0, cont["p.form.search"].data());
+            cCont["e.btnSearch.click"].trigger("click");
+        },
+        "e.gender.change": {
+            target: "#search #gender",
+            handler: function (e) {
+                cont["e.btnSearch.click"].trigger("click");
+            }
+        },
+        "e.eyeColor.change": {
+            target: "#search #eyeColor",
+            handler: function (e) {
+                cont["e.btnSearch.click"].trigger("click");
+            }
+        },
+        init: function (view, request) {
+        }
+    });
+})();
